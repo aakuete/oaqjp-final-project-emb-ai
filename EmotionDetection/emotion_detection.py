f0708cdbeefedc7bf5fd6b6e9b46917af40e0ecd
aakuete <kuetman@gmail.com>
2026-07-01 00:52:31 -0400

    Task 7 completed

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,11 +6,27 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers=header)
-    formatted_response = json.loads(response.text)
-    # Extracting and storing emotions into a dictionary called emotion  
-    emotion = formatted_response['emotionPredictions'][0]['emotion']
-    # Finding the dominant emotion based on the emotion with highest score
-    dominant_emotion = max(emotion, key=emotion.get)
-    # Storing the dominant emotion into emotion dictionary
-    emotion['dominant_emotion'] = dominant_emotion
+    emotion = {
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+            "joy": None,
+            "sadness": None,
+            "dominant_emotion": None
+            }
+    if response.status_code == 200:
+        formatted_response = json.loads(response.text)
+        # Extracting and storing emotions into a dictionary called emotion  
+        emotion = formatted_response['emotionPredictions'][0]['emotion']
+        # Finding the dominant emotion based on the emotion with highest score
+        dominant_emotion = max(emotion, key=emotion.get)
+        # Storing the dominant emotion into emotion dictionary
+        emotion['dominant_emotion'] = dominant_emotion
+    
+    elif response.status_code == 400:
+        emotion = {key: None for key in emotion}
+
+    else:
+        emotion = {key: None for key in emotion}
+
     return emotion
--- server.py
+++ server.py
@@ -8,9 +8,12 @@ def emot_detector():
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
     dominant_emot = response['dominant_emotion']
-    scores = {k: v for k, v in response.items() if k != "dominant_emotion"}
-    system_response = ", ".join([f"'{k}': {v}" for k, v in scores.items()])
-    return f"For the given statement, the system response is {system_response}. The dominant emoition is {dominant_emot}."
+    if dominant_emot == None:
+        return "Invalid text! Please try again!"
+    else:
+        scores = {k: v for k, v in response.items() if k != "dominant_emotion"}
+        system_response = ", ".join([f"'{k}': {v}" for k, v in scores.items()])
+        return f"For the given statement, the system response is {system_response}. The dominant emoition is {dominant_emot}."
 
 @app.route("/")
 def render_index_page():
