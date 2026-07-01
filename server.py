bc813b60583aec2111c9234732bc26c9d9755884
aakuete <kuetman@gmail.com>
2026-06-30 22:55:59 -0400

    Task 6 completed

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+#Initiate the flask app
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def emot_detector():
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+    dominant_emot = response['dominant_emotion']
+    scores = {k: v for k, v in response.items() if k != "dominant_emotion"}
+    system_response = ", ".join([f"'{k}': {v}" for k, v in scores.items()])
+    return f"For the given statement, the system response is {system_response}. The dominant emoition is {dominant_emot}."
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
