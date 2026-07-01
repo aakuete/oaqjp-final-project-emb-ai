89f01bfa5eda9606e7427823cb3f62d94c9b9eff
aakuete <kuetman@gmail.com>
2026-07-01 01:20:16 -0400

    Task 8 completed.

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+""" Importing libraries for the application."""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 #Initiate the flask app
@@ -5,19 +6,23 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emot_detector():
+    """ Function that anyalzes a string and return the corresponding emotion."""
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
     dominant_emot = response['dominant_emotion']
-    if dominant_emot == None:
+    if dominant_emot is None:
         return "Invalid text! Please try again!"
-    else:
-        scores = {k: v for k, v in response.items() if k != "dominant_emotion"}
-        system_response = ", ".join([f"'{k}': {v}" for k, v in scores.items()])
-        return f"For the given statement, the system response is {system_response}. The dominant emoition is {dominant_emot}."
+    scores = {k: v for k, v in response.items() if k != "dominant_emotion"}
+    system_response = ", ".join([f"'{k}': {v}" for k, v in scores.items()])
+    return (
+        f"For the given statement, the system response is {system_response}. \n" 
+        f"The dominant emoition is {dominant_emot}."
+        )
 
 @app.route("/")
 def render_index_page():
+    """ Function that render the application to an html."""
     return render_template('index.html')
-    
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
